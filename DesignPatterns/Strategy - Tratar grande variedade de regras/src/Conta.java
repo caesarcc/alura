ee2341946310fa2236db67a02f4231140e311ad3
Caesar Cesar <caesar-cesar@procergs.rs.gov.br>
2019-02-12 11:07:50 -0200

    Pattern para tratar quando há grande variedade de regras que podem ser aplicadas sobre um objeto individualmente, selecionadas pelo chamador, evitando ifs.

--- DesignPatterns/Strategy - Tratar grande variedade de regras/src/Conta.java
+++ DesignPatterns/Strategy - Tratar grande variedade de regras/src/Conta.java
@@ -1,8 +1,5 @@
 class Conta {
-	private String titular;
 	private double saldo;
-	private String numero;
-	private String agencia;
 
 	public void deposita(double valor) {
 		this.saldo += valor;
@@ -10,17 +7,5 @@ class Conta {
 
 	public double getSaldo() {
 		return this.saldo;
-	}
-
-	public String getTitular() {
-		return titular;
-	}
-
-	public String getNumero() {
-		return numero;
-	}
-
-	public String getAgencia() {
-		return agencia;
 	}
 }
